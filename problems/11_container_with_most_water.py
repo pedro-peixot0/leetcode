73d42707b0add06699cdea6b7ff29f6e6529fe89
pedro-peixot0 <101193174+pedro-peixot0@users.noreply.github.com>
2023-11-19 22:49:56 -0300

    last of weekend

---
+++ problems/11_container_with_most_water.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+
+        l, r = 0 , len(height) - 1
+        while l < r:
+            if height[l] < height[r]:
+                area = height[l] * (r - l)
+                l +=1
+            else:
+                area = height[r] * (r - l)
+                r -=1
+            
+            max_area = max(max_area, area)
+            
+        return max_area
